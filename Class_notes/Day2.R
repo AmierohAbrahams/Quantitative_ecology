403276e3a9d30fd10fbf96aeabfa6e027929be58
AmierohAbrahams <3559244@myuwc.ac.za>
2018-07-30 08:14:21 +0200

    updated_commit

---
+++ Class_notes/Day2.R
@@ -0,0 +1,13 @@
+# loading in the mite data
+
+# data("mite")
+# data(mite.env)
+# data("mite.xy")
+
+# re do everything in this analysis
+# Redo using tidyverse
+# Next week thursday
+# use vegan to load in the data
+
+
+# Homework
---
+++ Class_notes/Day_1.R
@@ -0,0 +1,20 @@
+# 
+# library(ncdf4)
+# Agulhas current most heat mre moist thus more rainffall and more diverse region.
+
+# find environmetnal datasets on the internet and discuss this datasts.
+# and possibly questions that could be asked with regrds to this dataset
+# metedate: how were they collected
+# spatoial resolution
+# how they were collectred
+# why was the data collected
+# what can be done with the data
+# why is it important 
+# alpha beta and gamma
+# pressentation with pp about ways in which an ecologist can collect data of ecological systems
+
+
+# eg quadrats, transects ectc
+
+
+
